68a3a2f08ff994ff0927df78b64a1f695d490e99
github username <scott.gloyna01@gmail.com>
2021-02-22 21:42:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance < 400){
+    return "This one is on me!";
+  } else if (distance >2500) {
+    return "No can do.";
+  } else if (distance > 2000 ){
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city == "NYC"){
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAssessment){
   // Write your code here!
+  if (tipAssessment == "generous"){
+    return "Thank you so much."
+  } else if ( tipAssessment =="not as generous"){
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
